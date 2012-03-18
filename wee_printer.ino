16516cd8b269d2003169be5d1d2f8b94f1f8ca16
James Adam <james@lazyatom.com>
2012-03-18 16:45:08 +0000

    More robust version of Arduino sketch.
    This implements a more comprehensive system for polling, downloading and finally printing an image. Once the polled URL has some content, it is downloaded, and the ready LED is lit. Pressing the button will then trigger printing.
    
    This is as a result of some fairly exhaustive testing on different networks with a stripped down sketch. On my home network, downloads are completely reliable from both local and remote servers.

--- wee_printer.ino
+++ wee_printer.ino
@@ -2,24 +2,33 @@
 #include <Ethernet.h>
 #include <SD.h>
 
-/* Includes for printer */
 #include <SoftwareSerial.h>
 #include <Thermal.h>
+#include <Bounce.h>
 
+#ifdef DEBUG
 #define debug(a) Serial.print(millis()); Serial.print(": "); Serial.println(a);
 #define debug2(a, b) Serial.print(millis()); Serial.print(": "); Serial.print(a); Serial.println(b);
+#else
+#define debug(a)
+#define debug2(a, b)
+#endif
 
-const int greenPin = 6;
-const int redPin = 7;
+const int errorLED = 5;
+const int downloadLED = 6;
+const int readyLED = 7;
 
 void initDiagnosticLEDs() {
-  pinMode(greenPin, OUTPUT);
-  pinMode(redPin, OUTPUT);
-  digitalWrite(greenPin, HIGH);
-  digitalWrite(redPin, HIGH);
-  delay(100);
-  digitalWrite(greenPin, LOW);
-  digitalWrite(redPin, LOW);
+  pinMode(errorLED, OUTPUT);
+  pinMode(downloadLED, OUTPUT);
+  pinMode(readyLED, OUTPUT);
+  digitalWrite(errorLED, HIGH);
+  digitalWrite(downloadLED, HIGH);
+  digitalWrite(readyLED, HIGH);
+  delay(1000);
+  digitalWrite(errorLED, LOW);
+  digitalWrite(downloadLED, LOW);
+  digitalWrite(readyLED, LOW);
 }
 
 const int printer_RX_Pin = 2;  // this is the green wire
@@ -32,16 +41,13 @@ void initPrinter() {
 }
 
 const int SD_Pin = 4;
-
 void initSD() {
-  // stop the SD stuff futzing.
   pinMode(SD_Pin, OUTPUT);
   SD.begin(SD_Pin);
 }
 
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x86, 0x67 }; //physical mac address
 EthernetClient client;
-
 void initNetwork() {
   // start the Ethernet connection:
   if (Ethernet.begin(mac) == 0) {
@@ -49,6 +55,7 @@ void initNetwork() {
     // no point in carrying on, so do nothing forevermore:
     while(true);
   }
+  delay(1000);
   // print your local IP address:
   debug2("IP address: ", Ethernet.localIP());
 }
@@ -56,80 +63,107 @@ void initNetwork() {
 void setup(){
   Serial.begin(9600);
 
-  initPrinter();
   initSD();
   initNetwork();
+  initPrinter();
   initDiagnosticLEDs();
 }
 
-const char* host = "192.168.1.22";
-const int port = 4567;
-const char *path = "/1";
+//const char* host = "192.168.1.22"; // uberatom
+//const char* host = "192.168.1.67"; // lazyatom
+//const char* host = "178.79.132.137"; // interblah
+const char* host = "interblah.net";
+const uint16_t port = 4567;
+const char *path = "/maybe";
 
-int failures = 0;
-int requests = 0;
+uint16_t failures = 0;
+uint16_t requests = 0;
+boolean downloadWaiting = false;
+char* cacheFilename = "TMP";
 
-void loop() {  
-  digitalWrite(greenPin, LOW);
+void checkForDownload() {
+  uint32_t length = 0;
+  uint32_t content_length = 0;
+
+  if (SD.exists(cacheFilename)) SD.remove(cacheFilename);
+  File cache = SD.open(cacheFilename, FILE_WRITE);
 
   if (client.connect(host, port)) {
-    requests++;
-    uint32_t content_length = 0;
-
-    debug("Connected.");
+    digitalWrite(downloadLED, HIGH);
     client.print("GET "); client.print(path); client.println(" HTTP/1.0\n"); 
-    uint32_t length = 0;
-    if (SD.exists("TMP")) { SD.remove("TMP"); }
-    File cache = SD.open("TMP", FILE_WRITE);
-
+    boolean parsingHeader = true;
+    unsigned long start = millis();
     while(client.connected()) {
       debug("Still connected");
-      // the server will close the connection when it is finished sending packets
-
-      client.find("Content-Length: ");
-      char c;
-      while (isdigit(c = client.read())) {
-        content_length = content_length*10 + (c - '0');
-      }
-      client.find("\r\n\r\n");
-      
-      debug2("Content length was: ", content_length);
-
-      debug("Now reading data");
       while(client.available()) {
-        // ...but not until this buffer is empty
-        cache.write(client.read());
-        length++;
+        if (parsingHeader) {
+          client.find("Content-Length: ");
+          char c;
+          while (isdigit(c = client.read())) {
+            content_length = content_length*10 + (c - '0');
+          }
+          debug2("Content length was: ", content_length);
+          client.find("\n\r\n"); // the first \r may already have been read above
+          parsingHeader = false;
+        } else {
+          cache.write(client.read());
+          length++;
+        }
       }
-      debug("Done reading for now...");
+      debug("No more data to read at the moment...");
     }
-    // close your end after the server closes its end
+
+    debug("Server has disconnected");
+    digitalWrite(downloadLED, LOW);
+    // Close the connection, and flush any unwritten bytes to the cache.
     client.stop();
+    cache.seek(0);
+    boolean success = (content_length == length) && (content_length == cache.size());
     cache.close();
 
-    cache = SD.open("TMP");
+    unsigned long duration = millis() - start;
+    debug2("Total bytes: ", length);
+    debug2("Duration: ", duration);
+    debug2("Speed: ", length/(duration/1000.0)); // NB - floating point math increases sketch size by ~2k
 
-    if (cache.size() != content_length) {
-      debug2("File size: ", cache.size());
-      failures++;
-    } else {
-      digitalWrite(greenPin, HIGH);
+    if (success) {
       if (content_length > 0) {
-        printer.printBitmap(&cache);
-        printer.feed(postPrintFeed);
+        downloadWaiting = true;
+        digitalWrite(readyLED, HIGH);
       }
+    } else {
+      failures++;
+      debug2("Oh no, a failure: ", failures);
+      digitalWrite(errorLED, HIGH);
+      digitalWrite(downloadLED, HIGH);
     }
-
-    cache.close();
-
-    if (failures > 0) {
-      digitalWrite(redPin, HIGH);
-    }
-    debug2("Total read: ", length);
-    Serial.print("Total failures: "); Serial.print(failures); Serial.print("/"); Serial.println(requests);
   } else {
-    debug("Couldn't connect.");
+    debug("Couldn't connect");
+    digitalWrite(errorLED, HIGH);
   }
+}
 
-  delay(1000);
+void printFromDownload() {
+  File cache = SD.open(cacheFilename);
+  printer.printBitmap(&cache);
+  printer.feed(postPrintFeed);
+  cache.close();
+}
+
+const int buttonPin = 8;
+Bounce bouncer = Bounce(buttonPin, 5); // 5 millisecond debounce
+unsigned long pollingDelay = 10000; // 1 minute
+
+void loop() {
+  if (downloadWaiting) {
+    bouncer.update();
+    if (bouncer.read() == HIGH) {
+      printFromDownload();
+      downloadWaiting = false;
+      digitalWrite(readyLED, LOW);
+    }
+  } else {
+    delay(pollingDelay);
+    checkForDownload();
+  }
 }
