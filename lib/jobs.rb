5f918c500f7c12bc4c3bd76cf49d1d71c59db5ff
James Adam <james@lazyatom.com>
2012-03-22 23:12:11 +0000

    This class is long gone.

--- lib/jobs.rb
+++ lib/jobs.rb
@@ -1,7 +1,6 @@
 module Jobs
   autoload :PreparePage, "jobs/prepare_page"
   autoload :ImageToBytes, "jobs/image_to_bytes"
-  autoload :Print, "jobs/print"
   autoload :Preview, "jobs/preview"
   autoload :PreviewReady, "jobs/preview_ready"
   autoload :PreviewContent, "jobs/preview_content"
