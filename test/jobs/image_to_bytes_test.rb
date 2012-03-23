31606b184fa8e059f659fbdac4446d701f318aea
James Adam <james@lazyatom.com>
2012-03-23 00:58:05 +0000

    Explicit requires are good.

--- test/jobs/image_to_bytes_test.rb
+++ test/jobs/image_to_bytes_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require "jobs"
 
 describe Jobs::ImageToBytes do
   describe "encoding data" do
--- test/jobs/prepare_page_test.rb
+++ test/jobs/prepare_page_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require "jobs"
 
 describe Jobs::PreparePage do
   describe "performing" do
