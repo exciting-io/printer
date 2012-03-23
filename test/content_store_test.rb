89ec6c45039cad4a0bdf83b460559908c687e424
James Adam <james@lazyatom.com>
2012-03-23 00:51:32 +0000

    It seems more correct that this be relative-url-like.

--- test/content_store_test.rb
+++ test/content_store_test.rb
@@ -5,23 +5,23 @@ describe ContentStore do
   it "wraps the given content in html tags" do
     ContentStore.write_html_content("content", "id")
     expected_html = %{<!doctype html><html class="no-js" lang="en">content</html>}
-    file_path = File.join(ContentStore.content_directory, "temp_content/id.html")
+    file_path = File.join(ContentStore.content_directory, "/temp_content/id.html")
     File.read(file_path).must_equal expected_html
   end
 
   it "stores the file using the id given" do
     ContentStore.write_html_content("content", "other-id")
-    File.exists?(File.join(ContentStore.content_directory, "temp_content/other-id.html")).must_equal true
+    File.exists?(File.join(ContentStore.content_directory, "/temp_content/other-id.html")).must_equal true
   end
 
   it "uses a default random id if no ID was given" do
     IdGenerator.stubs(:random_id).returns("default-id")
     ContentStore.write_html_content("content")
-    File.exists?(File.join(ContentStore.content_directory, "temp_content/default-id.html")).must_equal true
+    File.exists?(File.join(ContentStore.content_directory, "/temp_content/default-id.html")).must_equal true
   end
 
   it "returns the public path for the file" do
     path = ContentStore.write_html_content("content", "id")
-    path.must_equal "temp_content/id.html"
+    path.must_equal "/temp_content/id.html"
   end
 end
