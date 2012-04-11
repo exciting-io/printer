35f9f5a8814fb3ed1dc8494b5f6c7d22fd26c4af
James Adam <james@lazyatom.com>
2012-04-12 00:57:05 +0100

    I wonder if anyone will actually look at this.

--- lib/backend_server.rb
+++ lib/backend_server.rb
@@ -82,6 +82,21 @@ class PrinterBackendServer < Sinatra::Base
     erb :test_page, layout: :print_layout
   end
 
+  helpers do
+    def analytics
+      if ENV["ANALYTICS"]
+        %{<script type="text/javascript">
+          var _gaq = _gaq || []; _gaq.push(['_setAccount', '#{ENV["ANALYTICS"]}']); _gaq.push(['_trackPageview']);
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+        </script>}
+      end
+    end
+  end
+
   private
 
   def remote_printer_params(params)
