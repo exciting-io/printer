7b7b7d236a44c4c4d282025c9fe8ad383575e35a
James Adam <james@lazyatom.com>
2012-04-12 00:59:14 +0100

    Try to remote the javascript error.

--- lib/backend_server.rb
+++ lib/backend_server.rb
@@ -85,14 +85,18 @@ class PrinterBackendServer < Sinatra::Base
   helpers do
     def analytics
       if ENV["ANALYTICS"]
-        %{<script type="text/javascript">
-          var _gaq = _gaq || []; _gaq.push(['_setAccount', '#{ENV["ANALYTICS"]}']); _gaq.push(['_trackPageview']);
-          (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-          })();
-        </script>}
+        %{
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '#{ENV["ANALYTICS"]}']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+        }.strip
       end
     end
   end
