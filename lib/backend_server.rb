68484a7539e78e7bec12e9066bd3b2a96987d940
James Adam <james@lazyatom.com>
2012-04-12 01:05:52 +0100

    Remove the extra whitespace now we found the problem.

--- lib/backend_server.rb
+++ lib/backend_server.rb
@@ -85,18 +85,14 @@ class PrinterBackendServer < Sinatra::Base
   helpers do
     def analytics
       if ENV["ANALYTICS"]
-        %{
-<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', '#{ENV["ANALYTICS"].strip}']);
-  _gaq.push(['_trackPageview']);
+        %{<script type="text/javascript">
+  var _gaq = _gaq || []; _gaq.push(['_setAccount', '#{ENV["ANALYTICS"].strip}']); _gaq.push(['_trackPageview']);
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-</script>
-        }.strip
+</script>}.strip
       end
     end
   end
