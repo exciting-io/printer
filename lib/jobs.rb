f3454d69664238f0ca34848a40f24c7dc0f19b0f
James Adam <james@lazyatom.com>
2012-04-27 01:23:39 +0100

    Remove jobs that no longer exist.

--- lib/jobs.rb
+++ lib/jobs.rb
@@ -1,8 +1,5 @@
 module Jobs
   autoload :ImageToBits, "jobs/image_to_bits"
-  autoload :PrepareContent, "jobs/prepare_content"
   autoload :PreparePage, "jobs/prepare_page"
   autoload :Preview, "jobs/preview"
-  autoload :PreviewContent, "jobs/preview_content"
-  autoload :PreviewReady, "jobs/preview_ready"
 end
--- lib/jobs/preview_ready.rb
+++
@@ -1,7 +0,0 @@
-class Jobs::PreviewReady
-  class << self
-    def queue(preview_id)
-      "printer_preview_#{preview_id}"
-    end
-  end
-end
