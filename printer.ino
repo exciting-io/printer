3916c182d27131f8f62ea2a39f560ef6d60ad22a
James Adam <james@lazyatom.com>
2012-06-08 00:14:22 +0100

    Bump sketch version.
    This should reflect the new 'flush' fix.

--- printer.ino
+++ printer.ino
@@ -39,7 +39,7 @@ const byte SD_Pin = 4;         // the SD Card SPI pin
 
 // -- Everything below here can be left alone
 
-const char* sketchVersion = "1.0.1";
+const char* sketchVersion = "1.0.2";
 
 // -- Debugging
 
