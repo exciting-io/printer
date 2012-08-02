164bc906f2cb3cea1cad13a88dec9872280b5870
Chris Roos <chris@seagul.co.uk>
2012-08-02 10:07:36 +0100

    Bump version to 1.0.5

--- printer.ino
+++ printer.ino
@@ -39,7 +39,7 @@ const byte SD_Pin = 4;         // the SD Card SPI pin
 
 // -- Everything below here can be left alone
 
-const char sketchVersion[] = "1.0.4";
+const char sketchVersion[] = "1.0.5";
 
 // -- Debugging
 
