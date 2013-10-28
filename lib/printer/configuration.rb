f9c675f1b6c65fab9562ed4b1f5d1c0286b81566
James Adam <james@lazyatom.com>
2013-10-28 13:48:27 -0500

    Configure resque host accordingly.

--- lib/printer/configuration.rb
+++ lib/printer/configuration.rb
@@ -1,5 +1,7 @@
 require "data_mapper"
 require 'dm-timestamps'
+require "printer/data_store"
+require "resque"
 
 # DataMapper::Logger.new(STDOUT, :debug)
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/printer')
@@ -10,3 +12,5 @@ require "printer/print"
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
+
+Resque.redis = "#{Printer::DataStore.redis_host}:#{Printer::DataStore.redis_port}"
