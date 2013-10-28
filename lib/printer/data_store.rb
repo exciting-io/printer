51ed98b68163c3bfc24f9e1e4fe13d758ae2eb89
James Adam <james@lazyatom.com>
2013-10-28 13:26:53 -0500

    Allow configuration of redis connection.

--- lib/printer/data_store.rb
+++ lib/printer/data_store.rb
@@ -3,7 +3,15 @@ require "redis"
 require "redis/namespace"
 
 module Printer::DataStore
+  def self.redis_host
+    ENV["REDIS_HOST"] || "localhost"
+  end
+
+  def self.redis_port
+    ENV["REDIS_PORT"] || "6379"
+  end
+
   def self.redis
-    @redis ||= Redis::Namespace.new(:printer, redis: Redis.new)
+    @redis ||= Redis::Namespace.new(:printer, redis: Redis.new(host: redis_host, port: redis_port))
   end
 end
