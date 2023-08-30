0a1319541b9a361d753e8da2da426d03816438f9
James Adam <james@lazyatom.com>
2023-08-30 11:53:50 +0100

    Add very basic serial port test script

---
+++ pi/printer/basic-test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -o errexit
+set -o xtrace
+
+# exit if we are not root
+if [ "$(id -u)" != "0" ]; then
+  echo "This script must be run as root"
+  exit 1
+fi
+
+# check if the result of `raspi-config nonint get_serial_hw` is 0, then the serial port is enabled
+if [ "$(raspi-config nonint get_serial_hw)" = "0" ]; then
+  echo "Serial port is enabled"
+  echo "Sending test print data"
+  stty aF /dev/serial0 19200
+  echo "Testing basic serial output of printer\n\n" > /dev/serial0
+  echo "You should see a test print come out of the printer"
+else
+  echo "Serial port is disabled"
+  echo "Enabling serial port"
+  raspi-config nonint do_serial 2
+  echo "Please reboot and try again"
+  exit 1
+fi
